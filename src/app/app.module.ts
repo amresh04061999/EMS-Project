17ee3a9ee4cbdade670ce6dfe66d8570fb89cbd7
Amresh Prasad <amresh1098@gmail.com>
2023-02-10 18:39:20 +0530

    change folder-structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SharedComponent
   ],
   imports: [
     BrowserModule,

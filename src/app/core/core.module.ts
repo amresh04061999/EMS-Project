7f49ba71442092233118600869188762425f41b8
Amresh Prasad <amresh1098@gmail.com>
2023-02-10 18:33:40 +0530

    Created folder-structure

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    AsideComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }

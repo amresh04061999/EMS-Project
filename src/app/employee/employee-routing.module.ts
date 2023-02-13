948f693759ddbc6d0e94ad566739698cc3612289
Amresh Prasad <amresh1098@gmail.com>
2023-02-13 19:16:11 +0530

    feat:complte search button

--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -1,8 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChildren } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeComponent } from './employee.component';
 
-const routes: Routes = [{ path: '', component: EmployeeComponent }];
+const routes: Routes = [{
+  path: '', component: EmployeeComponent,
+  children: [{
+    path: '',
+    pathMatch: "full",
+    redirectTo: 'employe-list',
+  },
+  {
+    path: 'employe-list', component: EmployeeListComponent
+  }]
+},];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

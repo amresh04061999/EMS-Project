dc678f3f08ac1a7ac24c9d7b3828d1547499e9dd
Amresh Prasad <amresh1098@gmail.com>
2023-02-13 13:08:26 +0530

    feat:header-complete

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { AsideComponent } from './aside/aside.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +12,12 @@ import { AsideComponent } from './aside/aside.component';
     AsideComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    HeaderComponent,
+    AsideComponent
   ]
 })
 export class CoreModule { }

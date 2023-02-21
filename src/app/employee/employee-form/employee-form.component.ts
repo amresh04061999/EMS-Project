244f21d8c267a2c78332f49dc3d96a39541d6529
Amresh Prasad <amresh1098@gmail.com>
2023-02-21 11:56:15 +0530

    feat:image-preview Done

--- src/app/employee/employee-form/employee-form.component.ts
+++ src/app/employee/employee-form/employee-form.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
 import { OverlayService } from 'src/app/core/services/overlay.service';
-
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
   styleUrls: ['./employee-form.component.scss']
 })
 export class EmployeeFormComponent {
+  public imagefile!:File;
+  public  base64String:any
   constructor(private closeOverlay:OverlayService) { 
 
-  }
-  public close(){
-    this.closeOverlay.close()
 
+  }
+  /**
+   * images Select
+   * @param event 
+   */
+  public selectImage(event:any):void{
+    if(event){
+     this.imagefile=event.target.files[0]
+    }
+    let reader =new FileReader();
+    reader.readAsDataURL(this.imagefile);
+    reader.onload=()=>{
+    this.base64String=String(reader.result)
+      
+    }
+    
+  }
+  // close overlay
+  public close():void{
+    this.closeOverlay.close()
   }
 }

a8a84186a6b75b0fd91084788aa72db0898f944d
Amresh Prasad <amresh1098@gmail.com>
2023-02-21 19:12:31 +0530

    feat:getapi call

--- src/app/employee/employee-form/employee-form.component.ts
+++ src/app/employee/employee-form/employee-form.component.ts
@@ -1,35 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { City, Country, State } from 'country-state-city';
 import { OverlayService } from 'src/app/core/services/overlay.service';
 @Component({
   selector: 'app-employee-form',
   templateUrl: './employee-form.component.html',
   styleUrls: ['./employee-form.component.scss']
 })
-export class EmployeeFormComponent {
-  public imagefile!:File;
-  public  base64String:any
-  constructor(private closeOverlay:OverlayService) { 
+export class EmployeeFormComponent implements OnInit {
+  public imagefile!: File;
+  public base64String!: string;
+  public employeeForm:FormGroup;
+  public countries: any=[];
+  public states: any =[];
+  public cities: any =[];
+  public state: any=[];
+  public city: any =[];
+  public educationType=[
+    {
+      id:1,education:"Post Graduate",
+    },
+    {
+      id:2,education:"Under Graduate",
+    }
+  ]
+  constructor(private closeOverlay: OverlayService,private fb:FormBuilder) {
 
-
+   this.employeeForm=this.fb.group({
+    firstname:['',[Validators.required]],
+    lastname:['',[Validators.required]],
+    dob:['',[Validators.required]],
+    gender:['',[Validators.required]],
+    educationtype:['',[Validators.required]],
+    country:['',[Validators.required]],
+    state:['',[Validators.required]],
+    city:['',[Validators.required]],
+    Companyname:['',[Validators.required]],
+    Experience:['',[Validators.required]],
+    salry:['',[Validators.required]],
+    })
   }
+  ngOnInit(): void {
+    // get  all country
+    this.countries = Country.getAllCountries();
+    this.states = State.getAllStates();
+    this.cities = City.getAllCities()
+    console.log(this.countries);
+    
+  }
+// Save Employee data
+
+public saveEmployeeData():void{
+
+
+}
+
+
+
+
+
+
+
+
   /**
    * images Select
    * @param event 
    */
-  public selectImage(event:any):void{
-    if(event){
-     this.imagefile=event.target.files[0]
+
+  public selectImage(event: any): void {
+    if (event) {
+      this.imagefile = event.target.files[0]
     }
-    let reader =new FileReader();
+    let reader = new FileReader();
     reader.readAsDataURL(this.imagefile);
-    reader.onload=()=>{
-    this.base64String=String(reader.result)
-      
+    reader.onload = () => {
+      this.base64String = String(reader.result)
     }
-    
+  }
+  // Dependency   
+  /**
+   * Select country Dependency state
+   * @param event 
+   */
+  public onCountryChange(event: any): void {
+   let eventString: string = event.target.value.toUpperCase()
+   console.log(eventString);
+   
+   this.state= this.states.filter((res: any) => eventString === res.countryCode);
+  }
+  /**
+   * Select state Dependency city
+   * @param event 
+   */
+  public onStateChange(event: any): void {
+       let eventString=event.target.value;
+       this.city = this.cities.filter((res: any) => eventString === res.stateCode);
   }
   // close overlay
-  public close():void{
+  public close(): void {
     this.closeOverlay.close()
   }
 }
--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -5,6 +5,7 @@ import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeComponent } from './employee.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +16,9 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
   ],
   imports: [
     CommonModule,
-    EmployeeRoutingModule
+    EmployeeRoutingModule,
+    ReactiveFormsModule,
+    
   ]
 })
 export class EmployeeModule { }
